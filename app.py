139f62f3a183ae6d61db2d7a9fdabe167db708db
raystark04 <raymondgarcia1019@gmail.com>
2024-09-20 19:41:54 -0600

    login y register responsive, ya agrega clientes desde el register

--- app.py
+++ app.py
@@ -13,17 +13,71 @@ load_dotenv()
 app = Flask(__name__)
 
 
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+#Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
 @app.route('/')
 def index():
-    return render_template('iniciar_sesion.html')
+    return render_template('index.html')
+
+
+@app.route('/login', methods = ["GET", "POST"])
+def login():
+    if request.method == "GET":
+        return render_template('iniciar_sesion.html')
+    else:
+        userEmail = request.form.get('userEmail')
+        userPassword = request.form.get('userPassword')
+
+
+@app.route('/register', methods=["GET", "POST"])
+def registrarse():
+    if request.method == "GET":
+        return render_template('registrarse.html')
+    else:
+        userName = request.form.get('userName')
+        userEmail = request.form.get('email')
+        userPassword = request.form.get('password')
+        userConfirmPassword = request.form.get('confirm_password')
+
+        if not userName:
+            return render_template("registrarse.html", error_msg = "El nombre de usuario es requerido")
+        elif not userEmail:
+            return render_template("registrarse.html", error_msg = "El correo electrónico es requerido")
+        elif not userPassword:
+            return render_template("registrarse.html", error_msg = "Campo contraseña es requerido")
+        elif not userConfirmPassword:
+            return render_template("registrarse.html", error_msg = "Campo confirmar la contraseña es requerido")
+        elif userPassword != userConfirmPassword:
+            return render_template("registrarse.html", error_msg = "Las contraseñas no coinciden")
+        else:
+            hash_password = generate_password_hash(userPassword)
+        
+        duplicate_user = text('SELECT * FROM public."usuarios" WHERE "correo"=:correo')
+
+        if db.execute(duplicate_user,{'correo': userEmail}).rowcount > 0:
+            return render_template("registrarse.html", error_msg = "Un usuario como este ya existe, intenta nuevamente")
+        else:
+            new_user_client_query = text("INSERT INTO usuarios (nombre_completo, correo, clave, rol_id) VALUES (:userName, :userEmail, :userPassword, 3)")
+            db.execute(new_user_client_query,{"userName":userName, "userEmail":userEmail, "userPassword": hash_password})
+            db.commit()
+            db.close()
+            return redirect("/")
 
 @app.route('/categorias')
 def categorias():
     return render_template('categorias.html')
 
-@app.route('/register')
-def registrarse():
-    return render_template('registrarse.html')
+# @app.route('/index')
+# def index():
+#     return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8000, debug=True)
